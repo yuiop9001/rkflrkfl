7b27a75724da394fdb2d09c34a571f4c0a3b1255
이종 <whd9001@daum.net>
2018-10-04 17:19:08 +0900

    test 1

---
+++ project/연습2.js
@@ -0,0 +1,6 @@
+async function findAndSaveUser(uesr){
+    let uesr = await URLSearchParams.findOne({});
+    user.name='zero';
+    user=await user.save();
+    user=await User.findOne({gender:'m'});
+}
