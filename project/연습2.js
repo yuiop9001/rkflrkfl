ea4adfa7bae76dfc9224cc4d1dfc805e7e6e5b4d
이종 <whd9001@daum.net>
2018-10-04 18:01:14 +0900

    연습2

--- project/연습2.js
+++ project/연습2.js
@@ -2,5 +2,7 @@ async function findAndSaveUser(uesr){
     let uesr = await URLSearchParams.findOne({});
     user.name='zero';
     user=await user.save();
-    user=await User.findOne({gender:'m'});
+    user=await User.findOne({gender:'m'})
+    ;;;;
+    ;
 }
